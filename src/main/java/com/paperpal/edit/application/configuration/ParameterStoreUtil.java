e081fd320cc959764d7eb39483da6acbc27766b8
bajrang <bajrang.kumar@cactusglobal.com>
2024-09-23 10:23:38 +0530

    passing database host url as enviroment variable

--- src/main/java/com/paperpal/edit/application/configuration/ParameterStoreUtil.java
+++ src/main/java/com/paperpal/edit/application/configuration/ParameterStoreUtil.java
@@ -14,19 +14,12 @@ public class ParameterStoreUtil {
     private static final String PASSWORD_PARAMETER_PATH = "/config/service-edit/spring/datasource/password";
     private static final String USERNAME_PARAMETER_PATH = "/config/service-edit/spring/datasource/username";
 
-    private static final String LOCAL_DB_URL = "jdbc:postgresql://localhost:5123/paperpal-live-dev";
-    private static final String DB_URL = "jdbc:postgresql://dev-edit-microservices-paperpal-edit.ctnr4uiu21vf.us-east-1.rds.amazonaws.com:5432/edit";
-
     public static String getParameter(String name) {
         logger.info("fetching parameter value");
         AWSSimpleSystemsManagement ssmClient = AWSSimpleSystemsManagementClientBuilder.standard().build();
         GetParameterRequest request = new GetParameterRequest().withName(name).withWithDecryption(true);
         GetParameterResult result = ssmClient.getParameter(request);
         return result.getParameter().getValue();
-    }
-
-    public static String getDbUrl() {
-        return DB_URL;
     }
 
     public static String getDbUser() {
